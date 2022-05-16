ec6c4dddd36e93d188e3694927efb5a2a0e00f4f
TomMc608 <tommc9010@gmail.com>
2022-05-17 11:11:52 +1200

    gird was centered but dont know why the first image is smaller then the rest. The api is in the works but for som reson is not displaying an image. im going to use postman to try and find out whats happing

---
+++ assets/js/api.js
@@ -0,0 +1,24 @@
+const requestURL = 'https://api.unsplash.com/search/photos/?query=tea-leaves&client_id=QSz12piqsEZKwy7mseJIbTRYnc6SD1WgF91DNS7wp70';
+
+const get_image_button = document.querySelector('.get_image_button');
+
+const image_to_display = document.querySelector('.image_to_display');
+//When the button is clicked call getNewImage funtion 
+get_image_button.addEventListener('click', async () => {
+
+    let randomImage = await getNewImage();
+    image_to_display.scr = randomImage;
+});
+
+//get a random number between 1 and 10 get the data and wait 
+//then one it has the data use the random number to pick an image
+
+async function getNewImage() {
+    let randomNumber = Math.floor(Math.random() + 10);
+    return fetch(requestURL)
+        .then((response) => response.json())
+        .then((data) => {
+            let allImages = data.results[randomNumber];
+            return allImages.urls.regular;
+        });
+}
