b1cc7f64a49006232e60b3c338f6d1198c382722
TomMc608 <tommc9010@gmail.com>
2022-05-17 13:30:40 +1200

    api working but cant get an image to display not sure why but can console log data

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://api.unsplash.com/search/photos/?query=tea-leaves&client_id=QSz12piqsEZKwy7mseJIbTRYnc6SD1WgF91DNS7wp70';
+const URL = 'https://api.unsplash.com/search/photos/?query=tea-leaves&client_id=QSz12piqsEZKwy7mseJIbTRYnc6SD1WgF91DNS7wp70';
 
 const get_image_button = document.querySelector('.get_image_button');
 
@@ -6,19 +6,32 @@ const image_to_display = document.querySelector('.image_to_display');
 //When the button is clicked call getNewImage funtion 
 get_image_button.addEventListener('click', async () => {
 
-    let randomImage = await getNewImage();
-    image_to_display.scr = randomImage;
-});
+    fetch(URL)
+    .then(function (response){
+        console.log("show me the data");
+        console.log(response)
+        console.log("i am an android, lieutenant data");
+        return response.json();
+        
+    })
+    .then(function (jsonData) {
+        
+        image_to_display.src = jsonData.url;
+
+
+        
+
+    })
+
+})
+
+
+
+
+
+
+
+
 
 //get a random number between 1 and 10 get the data and wait 
 //then one it has the data use the random number to pick an image
-
-async function getNewImage() {
-    let randomNumber = Math.floor(Math.random() + 10);
-    return fetch(requestURL)
-        .then((response) => response.json())
-        .then((data) => {
-            let allImages = data.results[randomNumber];
-            return allImages.urls.regular;
-        });
-}
