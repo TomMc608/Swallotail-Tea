44e8ffc8f1d5251122136e5d4c9aa9825f651424
TomMc608 <tommc9010@gmail.com>
2022-05-18 18:17:00 +1200

    i now have got the button calling the api and getting a random image everytime it is clicked

--- assets/js/api.js
+++ assets/js/api.js
@@ -6,24 +6,23 @@ const image_to_display = document.querySelector('.image_to_display');
 //When the button is clicked call getNewImage funtion 
 get_image_button.addEventListener('click', async () => {
 
-    fetch(URL)
-    .then(response => response.json())
-    .then(data => {
-        console.log("show me the data");
-        ;
-        data["results"].forEach(result => {
-            image_to_display.src = (result["urls"]["regular"])
-            console.log(data)
-            console.log("i am an android, lieutenant data")
-        })
-    })
-
-        
+    let randomImage = await getNewImage();
+    image_to_display.src = randomImage;
+    
+ 
         
 })
 
 
-
+function getNewImage() {
+    let randomNumber = Math.floor(Math.random() * 10);
+    return fetch(URL)
+      .then((response) => response.json())
+      .then((data) => {
+        let allImages = data.results[randomNumber];
+        return allImages.urls.regular;
+      });
+  }
 
 
 
@@ -32,3 +31,17 @@ get_image_button.addEventListener('click', async () => {
 
 //get a random number between 1 and 10 get the data and wait 
 //then one it has the data use the random number to pick an image
+
+
+
+/* fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+        console.log("show me the data");
+        
+        data["results"].forEach(result => {
+            image_to_display.src = (result["urls"]["regular"])
+            console.log(data)
+            console.log("i am an android, lieutenant data")
+        })
+    }) */
