64b156aae0d84d0c79e17ff83a35c5938f67d61b
TomMc608 <tommc9010@gmail.com>
2022-06-06 17:32:11 +1200

    centered Join text and restructured tea.js files and css

---
+++ assets/js/tea.js
@@ -0,0 +1,24 @@
+let tea_grid = document.querySelector(".tea_grid");
+for(let i = 1; i <= 10; i++){
+fetch("https://api.unsplash.com/search/photos/?query=tea-leaves&client_id=QSz12piqsEZKwy7mseJIbTRYnc6SD1WgF91DNS7wp70")
+    .then(res => res.json())
+    .then(data => {
+    let randomNumber = Math.floor(Math.random() * 10); 
+    let images = data.results[randomNumber];
+    console.log(images.urls.regular)
+    let randomImage = images.urls.full;
+
+    let tea = document.createElement("div");
+    tea.classList.add("tea")
+
+    let image = document.createElement("div");
+    image.classList.add("image")
+    image.style.backgroundImage = `url(${randomImage})`;
+    tea.append(image)
+
+    tea_grid.append(tea)
+
+
+    
+    });
+};
