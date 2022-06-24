77f377c3e807eb12fff0e9b641d1b2705aeaa0fe
Tom <83677440+TomMc608@users.noreply.github.com>
2022-06-24 13:36:57 +1200

    Add:iv added the bottom bit of the page. Fix: iv fixed the js for the tea page. It was just becuase a variable was outside the funtion. So when i appened it it was null.

--- assets/js/tea.js
+++ assets/js/tea.js
@@ -1,4 +1,4 @@
-let tea_grid = document.querySelector('.tea_grid'); 
+
 for(let i = 1; i <= 17; i++){
 fetch("https://api.unsplash.com/search/photos/?query=tea-leaves&client_id=QSz12piqsEZKwy7mseJIbTRYnc6SD1WgF91DNS7wp70")
     .then(res => res.json())
@@ -10,7 +10,7 @@ fetch("https://api.unsplash.com/search/photos/?query=tea-leaves&client_id=QSz12p
 
     let tea = document.createElement("div");
     tea.classList.add("tea");
-
+    let tea_grid = document.querySelector('.tea_grid'); 
     let image = document.createElement("div");
     image.classList.add("image");
     image.style.backgroundImage = `url(${randomImage})`;
